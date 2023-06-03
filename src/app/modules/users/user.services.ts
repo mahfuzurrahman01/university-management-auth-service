9586ee9025ad221949620299c4f39a11fc7c26b1
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2023-06-03 11:44:25 +0600

    improve eslint setup

---
+++ src/app/modules/users/user.services.ts
@@ -0,0 +1,24 @@
+import config from '../../../config'
+import { IUser } from './user.interface'
+import { User } from './user.model'
+import generateUserId from './user.utills'
+
+const createUser = async (user: IUser): Promise<IUser | null> => {
+  const id = await generateUserId()
+  user.id = id as string
+  // giving a default password
+  if (!user.password) {
+    user.password = config.default_password as string
+  }
+
+  const createdUser = await User.create(user)
+
+  if (!createUser) {
+    throw new Error('Failed to create user!')
+  }
+  return createdUser
+}
+
+export default {
+  createUser,
+}
